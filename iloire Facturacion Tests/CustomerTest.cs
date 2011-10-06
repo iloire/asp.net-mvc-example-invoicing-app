68bfcd72f3851a8626c691f9d87c9e57c4c3b859
Ivan Loire <ivan@iloire.com>
2011-10-06 12:49:48 +0200

    Unit test project by using NUnit

---
+++ iloire Facturacion Tests/CustomerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using iloire_Facturacion.Controllers;
+using MvcPaging;
+
+namespace iloire_Facturacion_Tests
+{
+    [TestFixture]
+    public class CustomerTest
+    {
+        [Test]
+        public void TestListCustomers()
+        {
+            CustomerController c = new CustomerController();
+            System.Web.Mvc.ViewResult result = c.Index(null);
+            Assert.IsInstanceOf(typeof(IPagedList<Customer>), result.ViewData.Model);
+            Assert.IsNotNull(result.ViewData.Model);
+            //Assert.AreEqual(result.ViewName, "Index");
+        }
+    }
+}
+
