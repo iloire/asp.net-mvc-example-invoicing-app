7f49bb26db939f3186dee11403d11e70eab6f744
Ivan Loire <ivan@iloire.com>
2011-10-06 12:50:18 +0200

    Custom HandleErrorAttribute functionality to include Elmah signal

---
+++ iloire Facturacion/Models/Attributes/HandleErrorAttribute.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Elmah;
+
+namespace iloire_Facturacion.Models.Attributes
+{
+    public class HandleErrorAttribute : System.Web.Mvc.HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            base.OnException(context);
+
+            var e = context.Exception;
+            if (!context.ExceptionHandled   // if unhandled, will be logged anyhow
+                || RaiseErrorSignal(e)      // prefer signaling, if possible
+                || IsFiltered(context))     // filtered?
+                return;
+
+            LogException(e);
+        }
+
+        private static bool RaiseErrorSignal(Exception e)
+        {
+            var context = HttpContext.Current;
+            if (context == null)
+                return false;
+            var signal = ErrorSignal.FromContext(context);
+            if (signal == null)
+                return false;
+            signal.Raise(e, context);
+            return true;
+        }
+
+        private static bool IsFiltered(ExceptionContext context)
+        {
+            var config = context.HttpContext.GetSection("elmah/errorFilter")
+                         as ErrorFilterConfiguration;
+
+            if (config == null)
+                return false;
+
+            var testContext = new ErrorFilterModule.AssertionHelperContext(
+                                      context.Exception, HttpContext.Current);
+
+            return config.Assertion.Test(testContext);
+        }
+
+        private static void LogException(Exception e)
+        {
+            var context = HttpContext.Current;
+            ErrorLog.GetDefault(context).Log(new Error(e, context));
+        }
+    }
+}
+
