8e8c3e6e74e02a68bfab3575c74bc3fb969fcbbc
Ivan Loire <ivan@iloire.com>
2011-10-11 13:16:11 +0200

    ModelView for reports

---
+++ iloire Facturacion/Models/POCO/ModelView/Summary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+public class Summary
+{
+    public DateTime From { get; set; }
+    public DateTime To { get; set; }
+
+    public List<Invoice> Invoices { get; set; }
+    public List<Purchase> Purchases { get; set; }
+
+    public decimal NetIncome { get; set; }
+    public decimal NetExpense { get; set; }
+    public decimal NetBenefit { get { return NetIncome - NetExpense; } }
+
+    public decimal VATBalance { get; set; }
+}
