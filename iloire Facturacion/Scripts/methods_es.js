1ff77a80912cd778cd58af79838b13c1471b3648
Ivan Loire <ivan@iloire.com>
2011-10-11 13:19:50 +0200

    Range method in spanish culture

--- iloire Facturacion/Scripts/methods_es.js
+++ iloire Facturacion/Scripts/methods_es.js
@@ -8,5 +8,12 @@ jQuery.extend(jQuery.validator.methods, {
     },
     number: function (value, element) {
         return this.optional(element) || /^-?(?:\d+|\d{1,3}(?:\.\d{3})+)(?:,\d+)?$/.test(value);
+    },
+    range: function (value, element, param) {
+        var val1 = parseInt(String(param[0]).replace(',', '.'), 10);
+        var val2 = parseInt(String(param[1]).replace(',', '.'),10);
+        var val = parseInt(String(value).replace(',', '.'),10);
+
+        return this.optional(element) || (val >= val1 && val <= val2);
     }
 });
