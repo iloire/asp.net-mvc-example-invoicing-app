dc3607d988dd0ab7552dd5f6d1a2cd67977a26f1
Ivan Loire <ivan@iloire.com>
2011-10-14 13:16:13 +0200

    PurchaseType entity

--- iloire Facturacion/Models/POCO/Purchase.cs
+++ iloire Facturacion/Models/POCO/Purchase.cs
@@ -12,7 +12,7 @@ public class Purchase
     [Range(0.1, 999999999, ErrorMessage = "Price must be between 1 and 999999999")]
     public decimal Price { get; set; }
 
-    [Range(0.01, 100.0, ErrorMessage = "VAT must be a % between 0.01 and 100")]
+    [Range(0.00, 100.0, ErrorMessage = "VAT must be a % between 0 and 100")]
     public decimal VAT { get; set; }
 
     public int ProviderID { get; set; }
@@ -23,6 +23,10 @@ public class Purchase
     [DisplayName("Created")]
     public DateTime TimeStamp { get; set; }
 
+    public int PurchaseTypeID { get; set; }
+    [DisplayName("Expense category")]
+    public virtual PurchaseType PurchaseType { get; set; }
+   
     #region Calculated fields
     public decimal SubTotal
     {
---
+++ iloire Facturacion/Models/POCO/PurchaseType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+public class PurchaseType
+{
+    public int PurchaseTypeID { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    public string Descr { get; set; }
+
+    public virtual ICollection<Purchase> Purchases { get; set; }
+}
