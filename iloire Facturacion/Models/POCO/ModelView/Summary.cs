b67cc2c19ac9fcd1d8fc4105cf37bed2a18b46f2
Ivan Loire <ivan@iloire.com>
2011-10-14 13:17:18 +0200

    New ModelView class for Quarter summary

--- iloire Facturacion/Models/POCO/ModelView/Summary.cs
+++ iloire Facturacion/Models/POCO/ModelView/Summary.cs
@@ -5,12 +5,16 @@ using System.ComponentModel;
 
 public class Summary
 {
+    public int Year { get; set; }
+
     public DateTime From { get; set; }
     public DateTime To { get; set; }
 
     public List<Invoice> Invoices { get; set; }
     public List<Purchase> Purchases { get; set; }
+    public List<PurchaseType> PurchaseTypes { get; set; }
 
+    public decimal AmountPaid { get; set; }
     public decimal NetIncome { get; set; }
     public decimal NetExpense { get; set; }
     public decimal NetBenefit { get { return NetIncome - NetExpense; } }
