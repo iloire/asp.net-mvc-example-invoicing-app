c60a71cc3cc356173e7f3e4b486bd88093eb1b76
Ivan Loire <ivan@iloire.com>
2011-10-21 10:14:10 +0200

    English traslation

--- iloire Facturacion/Controllers/AccountController.cs
+++ iloire Facturacion/Controllers/AccountController.cs
@@ -49,7 +49,7 @@ namespace iloire_Facturacion.Controllers
                 }
                 else
                 {
-                    ModelState.AddModelError("", "El nombre de usuario o la contraseña especificados son incorrectos.");
+                    ModelState.AddModelError("", "Wrong username/password");
                 }
             }
 
@@ -141,7 +141,7 @@ namespace iloire_Facturacion.Controllers
                 }
                 else
                 {
-                    ModelState.AddModelError("", "La contraseña actual es incorrecta o la nueva contraseña no es válida.");
+                    ModelState.AddModelError("", "Wrong password or new password is invalid");
                 }
             }
 
@@ -156,45 +156,5 @@ namespace iloire_Facturacion.Controllers
         {
             return View();
         }
-
-        #region Status Codes
-        private static string ErrorCodeToString(MembershipCreateStatus createStatus)
-        {
-            // Vaya a http://go.microsoft.com/fwlink/?LinkID=177550 para
-            // obtener una lista completa de códigos de estado.
-            switch (createStatus)
-            {
-                case MembershipCreateStatus.DuplicateUserName:
-                    return "El nombre de usuario ya existe. Escriba un nombre de usuario diferente.";
-
-                case MembershipCreateStatus.DuplicateEmail:
-                    return "Ya existe un nombre de usuario para esa dirección de correo electrónico. Escriba una dirección de correo electrónico diferente.";
-
-                case MembershipCreateStatus.InvalidPassword:
-                    return "La contraseña especificada no es válida. Escriba un valor de contraseña válido.";
-
-                case MembershipCreateStatus.InvalidEmail:
-                    return "La dirección de correo electrónico especificada no es válida. Compruebe el valor e inténtelo de nuevo.";
-
-                case MembershipCreateStatus.InvalidAnswer:
-                    return "La respuesta de recuperación de la contraseña especificada no es válida. Compruebe el valor e inténtelo de nuevo.";
-
-                case MembershipCreateStatus.InvalidQuestion:
-                    return "La pregunta de recuperación de la contraseña especificada no es válida. Compruebe el valor e inténtelo de nuevo.";
-
-                case MembershipCreateStatus.InvalidUserName:
-                    return "El nombre de usuario especificado no es válido. Compruebe el valor e inténtelo de nuevo.";
-
-                case MembershipCreateStatus.ProviderError:
-                    return "El proveedor de autenticación devolvió un error. Compruebe los datos especificados e inténtelo de nuevo. Si el problema continúa, póngase en contacto con el administrador del sistema.";
-
-                case MembershipCreateStatus.UserRejected:
-                    return "La solicitud de creación de usuario se ha cancelado. Compruebe los datos especificados e inténtelo de nuevo. Si el problema continúa, póngase en contacto con el administrador del sistema.";
-
-                default:
-                    return "Error desconocido. Compruebe los datos especificados e inténtelo de nuevo. Si el problema continúa, póngase en contacto con el administrador del sistema.";
-            }
-        }
-        #endregion
     }
 }
